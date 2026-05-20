bc1f50edeea1a5161b4562a6fe30690a3694ced1
borisgnanzim <borisgnanzim@gmail.com>
2026-05-20 20:06:16 +0200

    pokedex

---
+++ app/index.tsx
@@ -0,0 +1,69 @@
+import { Card } from "@/components/Card";
+import { PokemonCard } from "@/components/pokemon/PokemonCard";
+import { ThemedText } from "@/components/ThemedText";
+import { getPokemonId } from "@/functions/pokemon";
+import { useFetchQuery } from "@/hooks/useFetchQuery";
+import { useThemeColors } from "@/hooks/useThemeColors";
+import { Link } from "expo-router";
+import { ActivityIndicator, FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  const colors = useThemeColors();
+  const { data, isFetching } = useFetchQuery('/pokemon?limit=21');
+  const pokemons = data?.results ?? [];
+
+  return (
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: colors.tint }]}
+    >
+      <View style={styles.header}>
+        <Image source={require("@/assets/images/pokeball.png")} width={24} height={24} />
+        <ThemedText variant="headline" color="grayLight">Pokédex</ThemedText>
+      </View>
+      <Card style={ styles.body }>
+        <FlatList 
+          data={pokemons} 
+          numColumns={3}
+          contentContainerStyle={ [styles.gridGap, styles.list]}
+          columnWrapperStyle={styles.gridGap}
+          ListFooterComponent={
+            isFetching ? < ActivityIndicator color={colors.tint}/> : null
+          }
+          renderItem={
+            ({item}) => <PokemonCard id={getPokemonId(item.url)} name={item.name} style={{ flex: 1/3 }} />
+          }  
+          keyExtractor={(item) => item.url}
+        />
+      </Card>
+        
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //justifyContent: "center",
+    //alignItems: "center",
+    //backgroundColor: "darkslategray", 
+    padding: 4,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16,
+    padding: 12,
+  },
+  body: {
+    flex: 1,
+    //margin: 12,
+  },
+  gridGap : {
+    gap: 8,
+  },
+  list: {
+    padding: 12,
+  }
+})
+
---
+++ components/Card.tsx
@@ -0,0 +1,18 @@
+import { Shadows } from "@/constants/Shadows";
+import { useThemeColors } from "@/hooks/useThemeColors";
+import { View, ViewProps, ViewStyle } from "react-native";
+
+
+type Props = ViewProps
+export function Card ({style, ...rest}: Props) {
+    const colors = useThemeColors();
+    return <View style= {[style, styles, { backgroundColor: colors.grayWhite }] } {...rest} />
+
+}
+
+const styles = {
+    //backgroundColor: "#FFF",
+    borderRadius: 8,   
+    overflow: "hidden",
+    ...Shadows.dp2
+} satisfies ViewStyle
